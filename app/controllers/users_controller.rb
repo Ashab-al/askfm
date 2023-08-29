01611cd813fad05e98e5ff594baffe2aabb89ac4
Allen <allenonrails@zohomail.eu>
2023-08-29 08:06:52 +0300

    users/show users/edit users/create users/update users/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,52 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :create, :new]
+
   def index
     @users = User.all
   end
 
+  def show
+    @new_question = @user.questions.build
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "#{@user.name}, ваш аккаунт успешно создан!"
+      redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
-  def show
-    @user = User.friendly.find(params[:id])
-    @new_question = @user.questions.build
+  def update
+    if @user.update(update_user_params)
+      flash[:success] = "#{@user.name}, ваш профиль изменён!"
+      redirect_to action: 'show'
+    else
+      render 'edit'
+    end
+  end
+
+  private 
+
+  def load_user
+    @user ||= User.friendly.find params[:id]
+  end
+
+  def update_user_params
+    params.require(:user).permit(:name, :avatar_url)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username, :name, :password_confirmation)
   end
 end
