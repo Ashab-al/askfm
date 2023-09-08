1104db1a51bca13abbaa1c2b5b08c010f20f1fc3
Ashab-al <asxabal7@gmail.com>
2023-09-09 01:58:15 +0300

    generate migrate AddAuthorNameToQuestion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_24_051531) do
+ActiveRecord::Schema.define(version: 2023_09_08_225522) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2023_08_24_051531) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "author_name"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
