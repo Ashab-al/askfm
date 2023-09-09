d85131aead11b98e40c44e9034790f5ca4ddefb0
Ashab-al <asxabal7@gmail.com>
2023-09-09 04:05:45 +0300

    made a dz

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,10 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def new
+    
   end
 
   def edit
@@ -12,5 +14,43 @@ class QuestionsController < ApplicationController
   end
 
   def show
+  end
+
+  def update
+    @question = Question.find(params[:id])
+  
+    if @question.update(question_update_params)
+      # Обновление прошло успешно
+      redirect_to @question, notice: "Вопрос успешно обновлен"
+    else
+      # Обновление не удалось
+      render 'edit'
+    end
+  end
+  
+
+  def create
+    @question = Question.new(question_params)
+
+    if current_user
+      @question.user = current_user
+    end
+
+    if @question.save
+      redirect_back fallback_location: root_path, notice: "Вопрос успешно добавлен"
+    else
+      render 'new'
+    end
+  end
+  
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text, :author_name, :user_id)
+  end
+
+  def question_update_params
+    params.require(:question).permit(:answer)
   end
 end
