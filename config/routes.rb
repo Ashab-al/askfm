7e5c7285fd1795ff8e8f67033ec818e7445b1f17
Ashab-al <asxabal7@gmail.com>
2023-09-25 23:20:56 +0300

    to make db:migrate for post

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :questions
   resources :posts
 
-
   get 'sign_up' => 'users#new'
   get 'log_out' => 'sessions#destroy'
   get 'log_in'  => 'sessions#new'
